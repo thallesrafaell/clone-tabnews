feb8937bb1e864f01a37680787ff389f8b381cd4
Thalles Rafael <thallesrafaell@outlook.com>
2026-03-02 19:51:07 +0000

    feat: add index page

---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Bem-vindo ao Clone do TabNews!</h1>
+      <p>Este é um projeto de implementação para o curso.dev.</p>
+    </div>
+  );
+}
+
+export default Home;
